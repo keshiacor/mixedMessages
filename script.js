4ade14de86cbc0393d08985c4c836a89723265ca
kcfit <keshiacorioll@me.com>
2024-04-04 21:13:05 -0400

    finished mixed message script

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-/* this script will generate a series of messages for users*/ 
+/* this script will generate a series of messages for users at random*/ 
 function randomGenerator (num){
     return Math.floor(Math.random() * num)
 } 
-
 const messagesOfHope = {
     dailyAffirmation : ['I have all the strength you need!', 'I radiate joy', 
-    'I am fearfully and wonderfully made', 'I am beautiful', 'I am confident'],
-    motivationMessage: ['Just do it!', 'Nothing is impossible for you', 'Think big, make it happen'],
+    'I am fearfully and wonderfully made', 'I am beautiful', 'I am confident',
+    'I am light'],
+    motivationMessage: ['Just do it!', 'Nothing is impossible for you', 
+    'Think big, make it happen'],
     quotes: ['Every day is a chance to be better','There is joy in every journey',
     'When you focus on the good, the good gets better',
     'Work hard in silence,let your success be your noise']
-};
-
-function messageReturn(num){
-    /* IF STATEMENT HERE - if value inputted is greater than 4, 
-    return something like "No daily affirmation for today"*/
 }
 
-//call the function with all four options 
-messageReturn(1);
-messageReturn(2);
-messageReturn(3);
-messageReturn(4);
+let messageOfTheDay = [];
+
+
+    for (messages in messagesOfHope){
+      let nextMessage = randomGenerator(messagesOfHope[messages].length);
+      console.log(nextMessage);
+      if (nextMessage === 1 || nextMessage===0){
+        messageOfTheDay.push(`Your daily affirmation is: ${messagesOfHope[messages][nextMessage]}.`);
+      } else if(nextMessage === 2){
+        messageOfTheDay.push(`Here's a motivational message for you: ${messagesOfHope[messages][nextMessage]}.`);
+      }else if (nextMessage === 3){
+        messageOfTheDay.push(`Today's quote is: ${messagesOfHope[messages][nextMessage]}.`);
+      }else if (nextMessage === 4){
+        messageOfTheDay.push(`Say this: ${messagesOfHope[messages][nextMessage]}.`);
+      }else if (nextMessage === 5){
+        messageOfTheDay.push(`Read this out loud: ${messagesOfHope[messages][nextMessage]}.`);
+      }
+      else if (nextMessage === 6){
+        messageOfTheDay.push(`Today: ${messagesOfHope[messages][nextMessage]}.`);
+      }
+      else {
+        messageOfTheDay.push(`Sorry wrong input, please try again!.`);
+      }
+    }
+    
+    function messageReturn(){   
+        console.log(messageOfTheDay.join('\n'));
+}
+
+
+messageReturn(messageOfTheDay);
+
+
+
